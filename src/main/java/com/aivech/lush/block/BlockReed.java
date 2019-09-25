e040587124e4c43b9869e6a68f6255fbe973a8a5
Aivech <16143392+Aivech@users.noreply.github.com>
2019-09-24 23:06:18 -0400

    Cattail 2: electric boogaloo

--- src/main/java/com/aivech/lush/block/BlockReed.java
+++ src/main/java/com/aivech/lush/block/BlockReed.java
@@ -1,11 +1,9 @@
 package com.aivech.lush.block;
 
-import com.aivech.lush.item.ItemBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.item.Item;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.StringIdentifiable;
@@ -13,19 +11,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ViewableWorld;
 
 public class BlockReed extends BlockWaterPlant {
-    public static final EnumProperty position = EnumProperty.of("position",Position.class);
+    public static final EnumProperty POSITION = EnumProperty.of("position", Position.class);
 
-    public final ItemBase placer;
-
-    public BlockReed(String id, ItemBase placer) {
+    public BlockReed(String id) {
         super(id);
-
-        this.placer = placer;
-    }
-
-    @Override
-    public Item asItem() {
-        return placer;
+        setDefaultState(getStateFactory().getDefaultState().with(POSITION, Position.BOTTOM));
     }
 
     @Override
@@ -33,7 +23,7 @@ public class BlockReed extends BlockWaterPlant {
         FluidState fluidState = state.getFluidState();
         Block under = world.getBlockState(pos.down()).getBlock();
 
-        return fluidState.getFluid().matches(FluidTags.WATER) && fluidState.isStill() &&
+        return fluidState.getFluid().matches(FluidTags.WATER) && fluidState.isStill() && world.getBlockState(pos.up()).isAir() &&
                 (under.equals(Blocks.DIRT) || under.equals(Blocks.CLAY) || under.equals(Blocks.SAND));
     }
 
