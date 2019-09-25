8b01e46e189fd785459d9646c566a9149a28bfb5
Aivech <16143392+Aivech@users.noreply.github.com>
2019-09-25 01:54:25 -0400

    Attempting to stop breaking stuff?

--- src/main/java/com/aivech/lush/world/features/LushFeatures.java
+++ src/main/java/com/aivech/lush/world/features/LushFeatures.java
@@ -1,7 +1,6 @@
 package com.aivech.lush.world.features;
 
 import com.aivech.lush.Lush;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
@@ -10,12 +9,12 @@ public enum LushFeatures {
     REED_FEATURE(new LushReedFeature(), "reed_feature"),
     TEST_FEATURE(new TestFeature(DefaultFeatureConfig::deserialize), "test_feature");
 
-    public final Identifier id;
+    public final String id;
     public final Feature feature;
 
     LushFeatures(Feature feature, String id) {
         this.feature = feature;
-        this.id = new Identifier(Lush.MODID, id);
+        this.id = Lush.MODID + ":" + id;
     }
 
     public static void init() {
